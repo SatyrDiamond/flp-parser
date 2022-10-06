3297551c302ae133cb75d57abf359bbdc0946d64
Colby <109001272+SatyrDiamond@users.noreply.github.com>
2022-10-05 23:31:19 -0400

    Update flp_parser_20.py

--- flp_parser_20.py
+++ flp_parser_20.py
@@ -14,35 +14,35 @@ with open('flp_eventname.txt') as f:
 
 eventtable = []
 for line in lines:
-	idname = line.rstrip().split()
-	if len(idname) > 1:
-		eventtable.append([idname[0], idname[1]])
-	else:
-		eventtable.append([idname[0], '_unknown'])
+    idname = line.rstrip().split()
+    if len(idname) > 1:
+        eventtable.append([idname[0], idname[1]])
+    else:
+        eventtable.append([idname[0], '_unknown'])
 
 
 def readriffdata(riffbytebuffer, offset):
-	if isinstance(riffbytebuffer, (bytes, bytearray)) == True:
-		riffbytebuffer = bytearray2BytesIO(riffbytebuffer)
-	riffobjects = []
-	riffbytebuffer.seek(0,2)
-	filesize = riffbytebuffer.tell()
-	riffbytebuffer.seek(offset)
-	while filesize > riffbytebuffer.tell():
-		chunkname = riffbytebuffer.read(4)
-		chunksize = int.from_bytes(riffbytebuffer.read(4), "little")
-		chunkdata = riffbytebuffer.read(chunksize)
-		riffobjects.append([chunkname, chunkdata])
-	return riffobjects
+    if isinstance(riffbytebuffer, (bytes, bytearray)) == True:
+        riffbytebuffer = bytearray2BytesIO(riffbytebuffer)
+    riffobjects = []
+    riffbytebuffer.seek(0,2)
+    filesize = riffbytebuffer.tell()
+    riffbytebuffer.seek(offset)
+    while filesize > riffbytebuffer.tell():
+        chunkname = riffbytebuffer.read(4)
+        chunksize = int.from_bytes(riffbytebuffer.read(4), "little")
+        chunkdata = riffbytebuffer.read(chunksize)
+        riffobjects.append([chunkname, chunkdata])
+    return riffobjects
 
 
 def datablock(PatternNotes, bytesnumber):
-	notelistdata = BytesIO()
-	notelistdata.write(PatternNotes)
-	notelistdata_filesize = notelistdata.tell()
-	notelistdata.seek(0)
-	while notelistdata.tell() < notelistdata_filesize:
-		print('\t\t' + bytes(notelistdata.read(bytesnumber)).hex())
+    notelistdata = BytesIO()
+    notelistdata.write(PatternNotes)
+    notelistdata_filesize = notelistdata.tell()
+    notelistdata.seek(0)
+    while notelistdata.tell() < notelistdata_filesize:
+        print('\t\t' + bytes(notelistdata.read(bytesnumber)).hex())
 
 def parse_patternnotes(patternnotesbytes):
     notelistdata = BytesIO()
@@ -52,93 +52,142 @@ def parse_patternnotes(patternnotesbytes):
     notelistdata.seek(0)
     notelist = []
     while notelistdata.tell() < notelistdata_filesize:
-        print('\t Note:')
-        print('\t\t', end='')
-        print('pos:',int.from_bytes(notelistdata.read(4), "little"), end=', ')
-        print('flags:',int.from_bytes(notelistdata.read(2), "little"), end=', ')
-        print('rackch:',int.from_bytes(notelistdata.read(2), "little"), end=', ')
-        print('dur:',int.from_bytes(notelistdata.read(4), "little"), end=', ')
-        print('key:',int.from_bytes(notelistdata.read(4), "little"), end=', ')
-        print('finep:',int.from_bytes(notelistdata.read(1), "little"), end=', ')
-        print('u1:',int.from_bytes(notelistdata.read(1), "little"), end=', ')
-        print('rel:',int.from_bytes(notelistdata.read(1), "little"), end=', ')
-        print('midich:',int.from_bytes(notelistdata.read(1), "little"), end=', ')
-        print('pan:',int.from_bytes(notelistdata.read(1), "little"), end=', ')
-        print('velocity:',int.from_bytes(notelistdata.read(1), "little"), end=', ')
-        print('mod_x:',int.from_bytes(notelistdata.read(1), "little"), end=', ')
-        print('mod_y:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\tNote:')
+        print('\t\tPosition:',int.from_bytes(notelistdata.read(4), "little"))
+        print('\t\tFlags:',int.from_bytes(notelistdata.read(2), "little"))
+        print('\t\tRackChannel:',int.from_bytes(notelistdata.read(2), "little"))
+        print('\t\tDuration:',int.from_bytes(notelistdata.read(4), "little"))
+        print('\t\tKey:',int.from_bytes(notelistdata.read(4), "little"))
+        print('\t\tFinePitch:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\t\tUnknown:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\t\tRelease:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\t\tMIDIChannel:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\t\tPan:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\t\tVelocity:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\t\tModX:',int.from_bytes(notelistdata.read(1), "little"))
+        print('\t\tModY:',int.from_bytes(notelistdata.read(1), "little"))
 
 def parse_FLTrack(trackdata):
-	fltrackdata = BytesIO()
-	fltrackdata.write(trackdata)
-	fltrackdata.seek(0)
-	print('\t ID:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t Color:',fltrackdata.read(4).hex())
-	print('\t Icon:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t Enabled:',int.from_bytes(fltrackdata.read(1), "little"))
-	print('\t Height:',struct.unpack('<f', fltrackdata.read(4))[0])
-	print('\t LockedToContent:',int.from_bytes(fltrackdata.read(1), "little"))
-	print('\t Motion:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t Press:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t TriggerSync:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t Queued:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t Tolerant:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t PositionSync:',int.from_bytes(fltrackdata.read(4), "little"))
-	print('\t Grouped:',int.from_bytes(fltrackdata.read(1), "little"))
-	print('\t Locked:',int.from_bytes(fltrackdata.read(1), "little"))
+    fltrackdata = BytesIO()
+    fltrackdata.write(trackdata)
+    fltrackdata.seek(0)
+    print('\tID:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tColor:',fltrackdata.read(4).hex())
+    print('\tIcon:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tEnabled:',int.from_bytes(fltrackdata.read(1), "little"))
+    print('\tHeight:',struct.unpack('<f', fltrackdata.read(4))[0])
+    print('\tLockedToContent:',int.from_bytes(fltrackdata.read(1), "little"))
+    print('\tMotion:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tPress:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tTriggerSync:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tQueued:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tTolerant:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tPositionSync:',int.from_bytes(fltrackdata.read(4), "little"))
+    print('\tGrouped:',int.from_bytes(fltrackdata.read(1), "little"))
+    print('\tLocked:',int.from_bytes(fltrackdata.read(1), "little"))
+
+def parse_arr(arrdata):
+    flarrdata = BytesIO()
+    flarrdata.write(arrdata)
+    flarrdata.seek(0)
+    print('\tPosition:',int.from_bytes(flarrdata.read(4), "little"))
+    print('\tPatternBase:',int.from_bytes(flarrdata.read(2), "little"))
+    print('\tItemIndex:',int.from_bytes(flarrdata.read(2), "little"))
+    print('\tLength:',int.from_bytes(flarrdata.read(4), "little"))
+    print('\tTrackIndex:',int.from_bytes(flarrdata.read(4), "little"))
+    print('\tUnknown1:',int.from_bytes(flarrdata.read(2), "little"))
+    print('\tFlags:',int.from_bytes(flarrdata.read(2), "little"))
+    print('\tUnknown2:',int.from_bytes(flarrdata.read(4), "little"))
+    print('\tStartOffset:',int.from_bytes(flarrdata.read(4), "little"))
+    print('\tEndOffset:',int.from_bytes(flarrdata.read(4), "little"))
+
+def parse_automation(autodatabytes):
+    autodata = BytesIO()
+    autodata.write(autodatabytes)
+    autodata.seek(0,2)
+    autodata_filesize = autodata.tell()
+    autodata.seek(0)
+    while autodata.tell() < autodata_filesize:
+        print('\tPoint:')
+        print('\t\tPosition:',int.from_bytes(autodata.read(4), "little"))
+        print('\t\tRackChannel:',int.from_bytes(autodata.read(1), "little")+1)
+        print('\t\tControl:',autodata.read(3))
+        print('\t\tValue:',autodata.read(4))
+
+def parse_fxrouting(fxroutingbytes):
+    fxroutingdata = BytesIO()
+    fxroutingdata.write(fxroutingbytes)
+    fxroutingdata.seek(0,2)
+    fxroutingdata_filesize = fxroutingdata.tell()
+    fxroutingdata.seek(0)
+    fxcount = 0
+    print('\tRoutes: ',end='')
+    while fxroutingdata.tell() < fxroutingdata_filesize:
+        fxchannel = int.from_bytes(fxroutingdata.read(1), "little")
+        if fxchannel == 1:
+            print(fxcount,end=' ')
+        fxcount += 1 
+    print()
+
 
 def parse_flp_Events(riffobj):
-	eventdatasize = len(riffobj)
-	eventdatastream = BytesIO()
-	eventdatastream.write(riffobj)
-	eventdatastream.seek(0)
-	outputtable = []
-	while eventdatastream.tell() < int(eventdatasize):
-		event_id = int.from_bytes(eventdatastream.read(1), "little")
-		if event_id <= 63 and event_id >= 0: # int8
-			outputtable.append([event_id,int.from_bytes(eventdatastream.read(1), "little")])
-		if event_id <= 127 and event_id >= 64 : # int16
-			outputtable.append([event_id,int.from_bytes(eventdatastream.read(2), "little")])
-		if event_id <= 191 and event_id >= 128 : # int32
-			outputtable.append([event_id,int.from_bytes(eventdatastream.read(4), "little")])
-		if event_id <= 224 and event_id >= 192 : # text
-			eventpartdatasize = varint.decode_stream(eventdatastream)
-			eventpart = eventdatastream.read(eventpartdatasize)
-			outputtable.append([event_id,eventpart])
-		if event_id <= 255 and event_id >= 225 : # data
-			eventpartdatasize = varint.decode_stream(eventdatastream)
-			eventpart = eventdatastream.read(eventpartdatasize)
-			outputtable.append([event_id,eventpart])
-	return outputtable
+    eventdatasize = len(riffobj)
+    eventdatastream = BytesIO()
+    eventdatastream.write(riffobj)
+    eventdatastream.seek(0)
+    outputtable = []
+    while eventdatastream.tell() < int(eventdatasize):
+        event_id = int.from_bytes(eventdatastream.read(1), "little")
+        if event_id <= 63 and event_id >= 0: # int8
+            outputtable.append([event_id,int.from_bytes(eventdatastream.read(1), "little")])
+        if event_id <= 127 and event_id >= 64 : # int16
+            outputtable.append([event_id,int.from_bytes(eventdatastream.read(2), "little")])
+        if event_id <= 191 and event_id >= 128 : # int32
+            outputtable.append([event_id,int.from_bytes(eventdatastream.read(4), "little")])
+        if event_id <= 224 and event_id >= 192 : # text
+            eventpartdatasize = varint.decode_stream(eventdatastream)
+            eventpart = eventdatastream.read(eventpartdatasize)
+            outputtable.append([event_id,eventpart])
+        if event_id <= 255 and event_id >= 225 : # data
+            eventpartdatasize = varint.decode_stream(eventdatastream)
+            eventpart = eventdatastream.read(eventpartdatasize)
+            outputtable.append([event_id,eventpart])
+    return outputtable
 
 fileobject = open(args.input, 'rb')
 headername = fileobject.read(4)
 rifftable = readriffdata(fileobject, 0)
 for riffobj in rifftable:
-	#print(str(riffobj[0]) + str(len(riffobj[1])))
-	if riffobj[0] == b'FLhd':
-		#print(riffobj[1][4:5])
-		ppq = int.from_bytes(riffobj[1][4:5], "big")
-	if riffobj[0] == b'FLdt':
-		flpevents = parse_flp_Events(riffobj[1])
+    #print(str(riffobj[0]) + str(len(riffobj[1])))
+    if riffobj[0] == b'FLhd':
+        #print(riffobj[1][4:5])
+        ppq = int.from_bytes(riffobj[1][4:5], "big")
+    if riffobj[0] == b'FLdt':
+        flpevents = parse_flp_Events(riffobj[1])
 
 for flpevent in flpevents:
-	event_id = flpevent[0]
-	event_data = flpevent[1]
-	if event_id == 238:
-		print(lines[event_id].strip())
-		parse_FLTrack(flpevent[1])
-	elif event_id == 224:
-		print(lines[event_id].strip())
-		parse_patternnotes(flpevent[1])
-	elif event_id == 233: #PlayListItems
-		print(lines[event_id].strip())
-		datablock(event_data, 32)
-	elif event_id == 241: #strings, utf16
-		print(lines[event_id].strip())
-		print('\t'+event_data.decode('utf-16le').rstrip('\0'))
-	elif event_id <= 255 and event_id >= 192 : # text
-		print(lines[event_id].strip())
-		print('\t'+str(event_data.hex()))
-	else:
-		print(lines[event_id].strip()+";"+str(flpevent[1]))
+    event_id = flpevent[0]
+    event_data = flpevent[1]
+    if event_id == 238:
+        print(lines[event_id].strip())
+        parse_FLTrack(flpevent[1])
+    elif event_id == 224:
+        print(lines[event_id].strip())
+        parse_patternnotes(flpevent[1])
+    elif event_id == 223:
+        print(lines[event_id].strip())
+        parse_automation(flpevent[1])
+    elif event_id == 233:
+        print(lines[event_id].strip())
+        parse_arr(flpevent[1])
+    elif event_id == 233:
+        print(lines[event_id].strip())
+        parse_arr(flpevent[1])
+    elif event_id == 235:
+        print(lines[event_id].strip())
+        parse_fxrouting(flpevent[1])
+    elif event_id <= 255 and event_id >= 192 : # text
+        print(lines[event_id].strip())
+        print('\t'+str(event_data.hex()))
+    else:
+        print(lines[event_id].strip()+";"+str(flpevent[1]))
